0c0a83d30cb81cbaa9dd66c709adba632cdf0de3
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2025-06-11 17:43:11 -0400

    Uprev to v1.0.6-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,7 @@ android {
         applicationId = "com.sourajitk.ambient_music"
         minSdk = 34
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.5-alpha"
+        versionName = "1.0.6-alpha"
     }
     signingConfigs {
         val hasSigningEnv = System.getenv("SIGNING_KEYSTORE_PASSWORD") != null
