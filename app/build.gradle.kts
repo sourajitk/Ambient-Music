590a7f6880b293c6102d64ff3a4da2e263963126
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2025-06-11 20:44:03 -0400

    fixup! Uprev to v1.0.6-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
         applicationId = "com.sourajitk.ambient_music"
         minSdk = 34
         targetSdk = 35
+        versionCode = 1
         versionName = "1.0.6-alpha"
     }
     signingConfigs {
