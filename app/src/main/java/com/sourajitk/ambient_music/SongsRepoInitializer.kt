08308ac58b7cacbf2fb7eba5fe2efde1f53b58a5
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2025-06-28 10:49:44 -0400

    Discard `ACTION_PREPARE` approach due to reliability issues
    Partially reverts and tweaks f8c2e75367ef81177c91808aeaf585a0c4483603.
    
    On Android 12+, an app running in the background has very strict limitations on
    when it can start a foreground service. Crash occurs as because SongsRepoInitializer
    is trying to start MusicPlaybackService with the ACTION_PREPARE intent while your app
    is considered to be in the background. Added the log here for future reference.
    
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: FATAL EXCEPTION: DefaultDispatcher-worker-1
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: Process: com.sourajitk.ambient_music, PID: 4757
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: android.app.BackgroundServiceStartNotAllowedException: Not allowed to start service Intent { act=com.sourajitk.ambient_music.ACTION_PREPARE xflg=0x4 cmp=com.sourajitk.ambient_music/.MusicPlaybackService }: app is in background uid UidRecord{2aa32eb u0a430 CEM  bg:+2s913ms idle change:cached|procstate|procadj procs:0 seq(839,839)} caps=--------
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at android.app.ContextImpl.startServiceCommon(ContextImpl.java:2048)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at android.app.ContextImpl.startService(ContextImpl.java:2003)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at android.content.ContextWrapper.startService(ContextWrapper.java:855)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at U2.t.i(Unknown Source:63)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at U2.q.n(Unknown Source:17)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at c3.a.m(Unknown Source:7)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at t3.C.run(Unknown Source:116)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at android.os.Handler.handleCallback(Handler.java:995)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:103)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:248)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:338)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:9067)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:593)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:932)
    06-28 13:46:25.918  4757  4908 E AndroidRuntime: 	Suppressed: y3.f: [z{Cancelling}@702b386, Dispatchers.IO]# Please enter the commit message for your changes. Lines starting

--- app/src/main/java/com/sourajitk/ambient_music/SongsRepoInitializer.kt
+++ app/src/main/java/com/sourajitk/ambient_music/SongsRepoInitializer.kt
@@ -2,7 +2,6 @@ package com.sourajitk.ambient_music
 
 import android.app.Application
 import android.content.ComponentName
-import android.content.Intent
 import android.service.quicksettings.TileService
 import android.util.Log
 
@@ -16,11 +15,6 @@ class SongsRepoInitializer : Application() {
       Log.d(TAG, "SongRepo init finished. Success: $success, Status: $statusMessage")
       if (success) {
         Log.d(TAG, "SongRepo successfully loaded songs. Requesting tile update.")
-        val serviceIntent =
-          Intent(this, MusicPlaybackService::class.java).apply {
-            action = MusicPlaybackService.ACTION_PREPARE
-          }
-        startService(serviceIntent)
         TileService.requestListeningState(this, ComponentName(this, CalmQSTileService::class.java))
         TileService.requestListeningState(this, ComponentName(this, ChillQSTileService::class.java))
         TileService.requestListeningState(this, ComponentName(this, SleepQSTileService::class.java))
