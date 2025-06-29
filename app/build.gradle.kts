95ac115601cd686f9bcdfac4bcdccd2e76564d4f
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2025-06-29 17:35:28 -0400

    settings: Revamp the Settings UI with useful preferences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ spotless {
 
 dependencies {
     implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.media3.ui)
     implementation(libs.androidx.media3.session)
