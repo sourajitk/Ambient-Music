6486a70412d4f41dfb9a632790b2c47aaf7215b8
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2025-07-25 09:38:28 -0400

    Make `requestTileUpdate()` reusable and use it wherever needed

--- app/src/main/kotlin/com/sourajitk/ambient_music/data/SongsRepoInitializer.kt
+++ app/src/main/kotlin/com/sourajitk/ambient_music/data/SongsRepoInitializer.kt
@@ -4,15 +4,10 @@
 package com.sourajitk.ambient_music.data
 
 import android.app.Application
-import android.content.ComponentName
-import android.service.quicksettings.TileService
 import android.util.Log
-import com.sourajitk.ambient_music.tiles.CalmQSTileService
-import com.sourajitk.ambient_music.tiles.ChillQSTileService
-import com.sourajitk.ambient_music.tiles.FocusQSTileService
-import com.sourajitk.ambient_music.tiles.SleepQSTileService
 import com.sourajitk.ambient_music.ui.notification.checkForAppUpdates
 import com.sourajitk.ambient_music.ui.notification.createUpdateNotificationChannel
+import com.sourajitk.ambient_music.util.TileStateUtil
 import com.sourajitk.ambient_music.util.UpdateScheduler.scheduleUpdateChecks
 
 class SongsRepoInitializer : Application() {
@@ -22,19 +17,17 @@ class SongsRepoInitializer : Application() {
 
   override fun onCreate() {
     super.onCreate()
-    // Call the new top-level function to create the channel
+
     createUpdateNotificationChannel(this)
     checkForAppUpdates(this)
     scheduleUpdateChecks(this)
+
     Log.d(TAG, "Initializing SongsRepo & Fetching JSON")
     SongsRepo.initializeAndRefresh(applicationContext) { success, statusMessage ->
       Log.d(TAG, "SongsRepo init finished. Success: $success, Status: $statusMessage")
       if (success) {
         Log.d(TAG, "SongsRepo successfully loaded songs. Requesting tile update.")
-        TileService.requestListeningState(this, ComponentName(this, CalmQSTileService::class.java))
-        TileService.requestListeningState(this, ComponentName(this, ChillQSTileService::class.java))
-        TileService.requestListeningState(this, ComponentName(this, SleepQSTileService::class.java))
-        TileService.requestListeningState(this, ComponentName(this, FocusQSTileService::class.java))
+        TileStateUtil.requestTileUpdate(applicationContext)
       }
     }
   }
