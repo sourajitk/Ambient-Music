fca6d95634f545cf4235da4123c036c4a40d8a24
ralph950412 <ralph950412@gmail.com>
2025-07-27 01:10:35 +0800

    Prompt users to disable battery optimization

--- app/src/main/kotlin/com/sourajitk/ambient_music/MainActivity.kt
+++ app/src/main/kotlin/com/sourajitk/ambient_music/MainActivity.kt
@@ -5,10 +5,14 @@ package com.sourajitk.ambient_music
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -24,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.sourajitk.ambient_music.data.GitHubRelease
 import com.sourajitk.ambient_music.ui.dialog.UpdateInfoDialog
@@ -48,6 +53,11 @@ class MainActivity : ComponentActivity() {
       }
     }
 
+  private fun isIgnoringBatteryOptimizations(context: Context): Boolean {
+    val powerManager = context.applicationContext.getSystemService(Context.POWER_SERVICE) as PowerManager
+    return powerManager.isIgnoringBatteryOptimizations("com.sourajitk.ambient_music")
+  }
+
   @RequiresApi(Build.VERSION_CODES.TIRAMISU)
   @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
   @SuppressLint("SourceLockedOrientationActivity")
@@ -68,8 +78,12 @@ class MainActivity : ComponentActivity() {
     }
 
     setContent {
+      val context = LocalContext.current
+      if (!isIgnoringBatteryOptimizations(context)) {
+        val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS, ("package:" + BuildConfig.APPLICATION_ID).toUri())
+        context.startActivity(intent)
+      }
       AmbientMusicTheme {
-        val context = LocalContext.current
         var updateInfo by remember { mutableStateOf<GitHubRelease?>(null) }
         val windowSizeClass = calculateWindowSizeClass(this)
         LaunchedEffect(key1 = true) {
