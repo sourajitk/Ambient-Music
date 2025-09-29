0478186a386d9193033ee2e400e193d8fc594e0e
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2025-09-29 14:49:29 -0400

    Uprev to v3.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = commitCount
-        versionName = "2.2.1-$commitHash"
+        versionName = "3.0.0-$commitHash"
         resValue("string", "app_version", "\"${versionName}\"")
     }
     signingConfigs {
