866901e025e3c974312eb0ad1e1db8bbec197576
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2026-05-23 13:49:56 -0400

    Make the "Download Genre" text scalable based on scroll state

--- app/src/main/kotlin/com/sourajitk/ambient_music/ui/offlinemode/DownloadGenresActivity.kt
+++ app/src/main/kotlin/com/sourajitk/ambient_music/ui/offlinemode/DownloadGenresActivity.kt
@@ -47,8 +47,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.lerp
 import com.sourajitk.ambient_music.R
 import com.sourajitk.ambient_music.data.SongsRepo
 import com.sourajitk.ambient_music.data.offline.GenreDownloader
@@ -141,7 +143,19 @@ fun DownloadGenresScreen(
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         topBar = {
             LargeTopAppBar(
-                title = { Text(stringResource(R.string.download_genres_title)) },
+                title = { 
+                    val collapsedFraction = scrollBehavior.state.collapsedFraction
+                    val fontSize = lerp(
+                        MaterialTheme.typography.headlineLarge.fontSize,
+                        MaterialTheme.typography.titleLarge.fontSize,
+                        collapsedFraction
+                    )
+                    Text(
+                        text = stringResource(R.string.download_genres_title),
+                        fontSize = fontSize,
+                        fontWeight = FontWeight.Bold
+                    ) 
+                },
                 navigationIcon = {
                     FilledIconButton(
                         onClick = onNavigateBack,
