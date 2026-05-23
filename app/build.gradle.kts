391286982eaca962c20a86ee515614f8b0a910ad
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2026-05-23 13:40:40 -0400

    Uprev to v5.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         minSdk = 31
         targetSdk = 37
         versionCode = commitCount
-        versionName = "4.2.1-$commitHash"
+        versionName = "5.0.0-$commitHash"
         resValue("string", "app_version", "\"${versionName}\"")
     }
 
